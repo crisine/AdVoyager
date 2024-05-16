46154e28508cb6c97e023a5dfbe2f8e6eb6abf54
crisine <crisine@knou.ac.kr>
2024-05-16 21:43:24 +0900

    feat: Overview 화면에서 표시되는 게시물 미리보기 셀들에 대해 댓글과 좋아요 개수를 확인할 수 있도록 아이콘 및 레이블 추가 구현

--- AdVoyager/Present/Overview/View/Cell/PlanCollectionViewCell.swift
+++ AdVoyager/Present/Overview/View/Cell/PlanCollectionViewCell.swift
@@ -25,20 +25,16 @@ final class PlanCollectionViewCell: BaseCollectionViewCell {
     
     private let backView: UIView = {
         let view = UIView()
-        
         view.clipsToBounds = true
         view.layer.cornerRadius = 16
-        
         return view
     }()
-    
     private let thumbnailImageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleToFill
         view.layer.masksToBounds = true
         return view
     }()
-    
     private let titleLabel: UILabel = {
         let view = UILabel()
         view.text = "제목 없음"
@@ -48,11 +44,56 @@ final class PlanCollectionViewCell: BaseCollectionViewCell {
         return view
     }()
     
+    private let commentIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "bubble")
+        view.tintColor = .black
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    private let commentCountLabel: UILabel = {
+        let view = UILabel()
+        view.text = "0"
+        view.font = .systemFont(ofSize: 16)
+        return view
+    }()
+    private let likeIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "heart")
+        view.tintColor = .systemPink
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    private let likeCountLabel: UILabel = {
+        let view = UILabel()
+        view.text = "0"
+        view.font = .systemFont(ofSize: 16)
+        return view
+    }()
+    
+    private let bottomStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.distribution = .fillProportionally
+        view.spacing = .zero
+        view.alignment = .center
+        return view
+    }()
+    
     override func configureHierarchy() {
         contentView.addSubview(shadowView)
         
-        [thumbnailImageView, titleLabel].forEach {
+        [thumbnailImageView, 
+         titleLabel,
+         bottomStackView].forEach {
             backView.addSubview($0)
+        }
+        
+        [commentIconImageView,
+         commentCountLabel,
+         likeIconImageView,
+         likeCountLabel].forEach {
+            bottomStackView.addArrangedSubview($0)
         }
         
         contentView.addSubview(backView)
@@ -68,15 +109,37 @@ final class PlanCollectionViewCell: BaseCollectionViewCell {
         }
         
         thumbnailImageView.snp.makeConstraints { make in
-            make.top.equalTo(contentView)
+            make.top.equalTo(backView)
             make.horizontalEdges.equalTo(backView)
-            make.bottom.equalTo(contentView.snp.centerY).offset(16)
+            make.bottom.equalTo(backView.snp.centerY).offset(16)
         }
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(thumbnailImageView.snp.bottom).offset(8)
-            make.horizontalEdges.equalTo(contentView)
-            make.bottom.equalTo(contentView).offset(-8)
+            make.horizontalEdges.equalTo(backView)
+            make.height.equalTo(48)
+        }
+        
+        bottomStackView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.horizontalEdges.equalTo(backView)
+            make.bottom.equalTo(backView).offset(-8)
+        }
+        
+        commentIconImageView.snp.makeConstraints { make in
+            make.height.equalTo(bottomStackView).inset(4)
+        }
+        
+        commentCountLabel.snp.makeConstraints { make in
+            make.height.equalTo(bottomStackView).inset(4)
+        }
+        
+        likeIconImageView.snp.makeConstraints { make in
+            make.height.equalTo(bottomStackView).inset(4)
+        }
+        
+        likeCountLabel.snp.makeConstraints { make in
+            make.height.equalTo(bottomStackView).inset(4)
         }
     }
     
@@ -87,6 +150,8 @@ final class PlanCollectionViewCell: BaseCollectionViewCell {
     func updateCell(post: Post) {
         
         titleLabel.text = post.title
+        commentCountLabel.text = String(post.comments.count)
+        likeCountLabel.text = String(post.likes.count)
         
         let baseUrl = APIKey.baseURL.rawValue + "/"
         
